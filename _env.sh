f479d730ef4d7cac0afd425ca429ea5629d4e269
JGWill <jgi@jgwill.com>
2025-11-07 16:53:39 -0500

    session_id #60

---
+++ _env.sh
@@ -0,0 +1,41 @@
+
+
+#---START MIGRATD from .env
+session_id=598a16b1-6ada-429d-a587-9bf2d7b9646e
+issue_87_session_id=8dde9377-b261-4399-9048-0dbbe65d7b2e
+
+
+
+session_id_250930143427=314f51c5-36d6-44e3-bf0b-e0a78a58d5e4
+MCP_CONFIGS_FILES="/src/.mcp.STC-IAIP.storytelling-314f51c5-36d6-44e3-bf0b-e0a78a58d5e4.json"
+ADD_DIRS_PATHS="/w/orpheus_ART/ /src/IAIP/ /cesaret/book/_/tcc/ /w/CSV/kb2508"
+session_id_2509301518=f9739d98-7b3e-4eaf-a3e8-1257f8d91d38
+session_id_2509301518_SOUTH=8001afb5-27ac-42f5-956e-31282de665d2
+
+session_id_251006_issue_129_IAIP_AUTOETHNOGRAPHY_AND_CEREMONIAL_DIARIES=7910843e-57bf-4aee-8996-3cae458f6fef
+session_id_251006_issue_129_IAIP_AUTOETHNOGRAPHY_AND_CEREMONIAL_DIARIES_MCP_CONFIG_FILES="/src/.mcp.STC-IAIP.storytelling-314f51c5-36d6-44e3-bf0b-e0a78a58d5e4.json /src/.mcp.github.miadisabelle.json"
+session_id_251006_issue_130_mia_agents_5631e86759004d65865aff8b08e3bc3a="5631e867-5900-4d65-865a-ff8b08e3bc3a"
+
+#251008
+session_id_251008_issue_xyz_arena_DIR_ADDS="/b/Dropbox/jgt/edu/llm_mastery/arenas_250706 /cesaret/book/_/tcc/ /w/CSV/kb2508 /w/orpheus_ART/"
+session_id_251008_issue_xyz_arena_STCGOAL="Fragmented thinking made these directories /b/Dropbox/jgt/edu/llm_mastery/arenas_250706 /cesaret/book/_/tcc/ /w/CSV/kb2508 /w/orpheus_ART/ to occurs, the goal is explore them and we would unify the platform creation for a career I love and something I can present to the community.  They could be considered as implementing the same feature sets or being different packages that we would work on like in @jgwill/ python package."
+session_id_251008_issue_xyz_arena_UUID="0f5aaa1e-faea-4ec3-af79-a594fb3a271d"
+session_id_251008_issue_xyz_arena_TLID="2510081204"
+
+
+
+
+session_id_LAUNCH_IAIP_CLAUDE_4_DIRECTIONS_PROPOSAL_250926=5cf366c5-efb0-4212-8430-71cecd7d190e
+session_id_2510082009_follow_storytelling_session=3c8933e4-791b-42ef-85a6-98b4ab3e68dd
+session_id_jgwill_storytelling_251014=ddd138bd-28ce-4967-97b6-e83113efefde
+session_id_2510161024=df424a00-7514-4332-a240-63275d4ed831
+#---END MIGRATD from .env
+
+
+
+session_id__coaiapy_mcp_2510191020=08636648-9d35-490f-896d-70a2d52d5164
+
+
+session_id__issue_60__UUID=2ab5df9e-447e-467a-ad92-d136f6b8d018
+
+
